89d16eb72e08bb12c35f26b375e4f351a8e5fa6c
Antopato <90763997+Antopato@users.noreply.github.com>
2024-03-21 16:49:54 +0100

    LogIn and Register started

--- app/src/main/java/com/example/smarttrade/MainActivity.kt
+++ app/src/main/java/com/example/smarttrade/MainActivity.kt
@@ -1,6 +1,11 @@
 package com.example.smarttrade
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import com.example.smarttrade.MainActivity
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,11 +16,34 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.smarttrade.ui.theme.SmartTradeTheme
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
+
+        setContentView(R.layout.login)
+        val logInButt = findViewById<Button>(R.id.logIn)
+        val signIn = findViewById<TextView>(R.id.signIn)
+
+        logInButt.setOnClickListener{
+            val userText = findViewById<EditText>(R.id.username)
+            val passText = findViewById<EditText>(R.id.password)
+            logIn(userText.text.toString(), passText.text.toString())
+            println(userText.text.toString())
+        }
+
+        signIn.setOnClickListener() {
+            println("Has pulsado register")
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+
+
+        /*setContent {
             SmartTradeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -25,9 +53,18 @@ class MainActivity : ComponentActivity() {
                     Greeting("Android")
                 }
             }
-        }
+        }*/
+    }
+    fun logIn(name: String, pass:String){
+        /*if(checkLog()){
+            //move to main page
+        }*/
+
     }
 }
+
+
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
---
+++ app/src/main/java/com/example/smarttrade/RegisterActivity.kt
@@ -0,0 +1,15 @@
+package com.example.smarttrade
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+
+class RegisterActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+
+    }
+}
