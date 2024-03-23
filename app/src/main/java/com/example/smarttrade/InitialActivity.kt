361305ef59df42dc061efb3ee9cdb41f6a04bb80
Antopato <90763997+Antopato@users.noreply.github.com>
2024-03-23 19:02:30 +0100

    initial page added

---
+++ app/src/main/java/com/example/smarttrade/InitialActivity.kt
@@ -0,0 +1,30 @@
+package com.example.smarttrade
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class InitialActivity : AppCompatActivity(){
+
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.initial_page)
+
+        val logInButt = findViewById<Button>(R.id.logInButt)
+        val regCustButt = findViewById<Button>(R.id.regCusButt)
+
+        regCustButt.setOnClickListener() {
+            println("Has pulsado register")
+            startActivity(Intent(this, CostumerRegisterActivity::class.java))
+        }
+
+        logInButt.setOnClickListener(){
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+}
