4c803084092a836d6296f502490d6255d444a424
Mario Nu?ez Blazquez <MNUEBLA@upv.edu.es>
2024-04-02 13:47:05 +0200

    making costumer and merchant look alike

--- app/src/main/java/com/example/smarttrade/InitialActivity.kt
+++ app/src/main/java/com/example/smarttrade/InitialActivity.kt
@@ -16,7 +16,7 @@ class InitialActivity : AppCompatActivity(){
 
         val logInButt = findViewById<Button>(R.id.logInButt)
         val regCustButt = findViewById<Button>(R.id.regCusButt)
-        val regMerchButt = findViewById<Button>(R.id.reComButt)
+        val regComButt = findViewById<Button>(R.id.regComButt)
 
         regCustButt.setOnClickListener() {
             println("Has pulsado register")
@@ -24,11 +24,11 @@ class InitialActivity : AppCompatActivity(){
         }
 
         logInButt.setOnClickListener(){
-            startActivity(Intent(this, MainActivity::class.java))
+            startActivity(Intent(this, LogInActivity::class.java))
         }
 
-        regMerchButt.setOnClickListener(){
-            startActivity(Intent(this, MerchantRegisterActivity::class.java))
+        regComButt.setOnClickListener(){
+            startActivity(Intent(this, CompanyRegisterActivity::class.java))
         }
 
     }
