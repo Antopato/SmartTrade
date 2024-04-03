550158383c7ea521942615116a903e2bd8c91ea3
Mario Nu?ez Blazquez <MNUEBLA@upv.edu.es>
2024-04-03 13:48:58 +0200

    Nueva actividad AddProduct

---
+++ app/src/main/java/com/example/smarttrade/AddProductActivity.kt
@@ -0,0 +1,29 @@
+package com.example.smarttrade
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class AddProductActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.add_product)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val spinner : Spinner = findViewById(R.id.spinnerCategories)
+        val options = resources.getStringArray(R.array.options)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, options)
+        spinner.adapter = adapter
+    }
+
+
+
+}
