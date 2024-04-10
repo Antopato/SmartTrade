39464994cd0dd9897d3d2a43f570d16191d4be0a
Mario Nu?ez Blazquez <MNUEBLA@upv.edu.es>
2024-04-10 16:33:58 +0200

    Tamaño de botones ajustado e instroducido dependencias retrofit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,4 +71,8 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation ("com.squareup.picasso:picasso:2.71828")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
 }
