39ebec5ad9943af92a99bce39e4f8773163a2167
Alberto Melgar Garcia <AMELGAR1@upv.edu.es>
2024-05-30 17:27:30 +0200

    añadido patron Memento al adapter

--- app/src/main/java/com/example/smarttrade/adapters/CarAdapter.kt
+++ app/src/main/java/com/example/smarttrade/adapters/CarAdapter.kt
@@ -14,104 +14,101 @@ import com.example.smarttrade.BusinessLogic
 import com.example.smarttrade.ProductActivity
 import com.example.smarttrade.R
 import com.example.smarttrade.ShoppingCarActivity
-import com.example.smarttrade.classes.Sell
 import com.example.smarttrade.classes.ShoppingCart
 import com.example.smarttrade.classes.User
 
-class CarAdapter(val context: Context, val list: MutableList<ShoppingCart>, val observer : ShoppingCarActivity, val user : User) : RecyclerView.Adapter<CarAdapter.MyViewHolder>() {
+class CarAdapter(val context: Context, val list: MutableList<ShoppingCart>, val observer: ShoppingCarActivity, val user: User) : RecyclerView.Adapter<CarAdapter.MyViewHolder>() {
     var sum = 0
     val service = BusinessLogic()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val inflater = LayoutInflater.from(context)
-        val view =  inflater.inflate(R.layout.shopping_car_row, parent, false)
-        val viewHold = MyViewHolder(view,context,list)
-
-        return viewHold
+        val view = inflater.inflate(R.layout.shopping_car_row, parent, false)
+        return MyViewHolder(view, context, list)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val sell = service.getSellById(list.get(position).product_id)
+        val sell = service.getSellById(list[position].product_id)
         val product = service.getProductById(sell!!.id_product)
-        val userId = list.get(position).shopping_cart_owner
-        holder.price.setText(list.get(position).price.toString() + "€")
-        println(list.get(position).quantity.toString())
-        holder.amount.setText(list.get(position).quantity.toString())
-        holder.name.setText(product.name)
+        val userId = list[position].shopping_cart_owner
+        holder.price.text = "${list[position].price}€"
+        holder.amount.text = list[position].quantity.toString()
+        holder.name.text = product.name
 
-        val unitPrice = list.get(position).price
-        val amount = list.get(position).quantity
-        sum += unitPrice*amount
+        val unitPrice = list[position].price
+        val amount = list[position].quantity
+        sum += unitPrice * amount
         observer.change()
-        println(sum)
         service.setImage(holder.image, product)
         val image = service.getImageByType(product.productType, product.productId)
         holder.image.setImageBitmap(BitmapFactory.decodeByteArray(image, 0, image.size))
 
-        holder.minusButton.setOnClickListener(){
+        holder.minusButton.setOnClickListener {
+            observer.saveState()
             val number = holder.amount.text.toString().toInt()
-            if(number-1 >0 ){
-                holder.amount.text = (number-1).toString()
-                list.get(position).quantity = (number-1)
-                val shoppingCartId:Int = list.get(position).shoppingCart_id
-                var value = list.get(position).price
+            if (number - 1 > 0) {
+                holder.amount.text = (number - 1).toString()
+                list[position].quantity = (number - 1)
+                val shoppingCartId: Int = list[position].shoppingCart_id
+                val value = list[position].price
                 sum -= value
                 observer.change()
                 service.minusOne(shoppingCartId)
-                println(list.get(position).quantity)
+                notifyDataSetChanged()
             }
         }
 
-        holder.plusButton.setOnClickListener(){
-            val number = holder.amount.text.toString().toInt() +1
-            list.get(position).quantity = number
-            val shoppingCartId:Int = list.get(position).shoppingCart_id
+        holder.plusButton.setOnClickListener {
+            observer.saveState()
+            val number = holder.amount.text.toString().toInt() + 1
+            list[position].quantity = number
+            val shoppingCartId: Int = list[position].shoppingCart_id
             holder.amount.text = number.toString()
-            var value = list.get(position).price
-            sum+= value
+            val value = list[position].price
+            sum += value
             observer.change()
             service.plusOne(shoppingCartId)
-            println(list.get(position).quantity)
-        }
-        holder.laterButton.setOnClickListener(){
-            service.addToLaterList(product.productId,userId)
-            println("Añadido a forLater")
-            service.deleteProdFromCart(product.productId,userId)
-            println("Producto eliminado")
-            sum=0
-            observer.changeData(true, position)
-        }
-        holder.deleteButton.setOnClickListener(){
-            service.deleteProdFromCart(product.productId,userId)
-            sum=0
-            observer.changeData(false, position)
+            notifyDataSetChanged()
         }
 
+        holder.laterButton.setOnClickListener {
+            observer.saveState()
+            service.addToLaterList(product.productId, userId)
+            service.deleteProdFromCart(product.productId, userId)
+            sum = 0
+            observer.changeData(true, position)
+            notifyDataSetChanged()
+        }
+
+        holder.deleteButton.setOnClickListener {
+            observer.saveState()
+            service.deleteProdFromCart(product.productId, userId)
+            sum = 0
+            observer.changeData(false, position)
+            notifyDataSetChanged()
+        }
     }
 
     override fun getItemCount(): Int {
         return list.count()
     }
 
-    fun getTotal() : Int
-    {
-        println("He devuelto sum con un valor de "+ sum)
+    fun getTotal(): Int {
         return sum
     }
 
+    class MyViewHolder(itemView: View, context: Context, list: List<ShoppingCart>) : RecyclerView.ViewHolder(itemView) {
+        val price: TextView = itemView.findViewById(R.id.priceText)
+        val name: TextView = itemView.findViewById(R.id.listProductName)
+        val plusButton: Button = itemView.findViewById(R.id.plusButton)
+        val minusButton: Button = itemView.findViewById(R.id.minusButton)
+        val amount: TextView = itemView.findViewById(R.id.amountText)
+        val deleteButton: Button = itemView.findViewById(R.id.listDeleteButton)
+        val laterButton: Button = itemView.findViewById(R.id.forLaterButton)
+        val image: ImageView = itemView.findViewById(R.id.listProductImage)
 
-
-    class MyViewHolder(itemView: View, context: Context, list:List<ShoppingCart>) : RecyclerView.ViewHolder(itemView){
-        val price : TextView = itemView.findViewById(R.id.priceText)
-        val name : TextView = itemView.findViewById(R.id.listProductName)
-        val plusButton : Button = itemView.findViewById(R.id.plusButton)
-        val minusButton : Button = itemView.findViewById(R.id.minusButton)
-        val amount : TextView = itemView.findViewById(R.id.amountText)
-        val deleteButton : Button = itemView.findViewById(R.id.listDeleteButton)
-        val laterButton : Button = itemView.findViewById(R.id.forLaterButton)
-        val image : ImageView = itemView.findViewById(R.id.listProductImage)
-        init{
+        init {
             name.setOnClickListener {
-
                 val intent = Intent(context, ProductActivity::class.java)
                 //intent.putExtra("product", list.get(adapterPosition))
                 //val bitmap = (image.drawable).toBitmap()
@@ -120,14 +117,7 @@ class CarAdapter(val context: Context, val list: MutableList<ShoppingCart>, val 
                 //val byteArray = stream.toByteArray()
                 //intent.putExtra("image", byteArray)
                 context.startActivity(intent)
-
-
             }
         }
-
-
-
-
     }
-
 }
